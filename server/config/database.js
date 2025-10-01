b929169652c4d39b71359f3df20e9c302646e0fd
chisaiki <mishitarahman@gmail.com>
2025-10-01 15:43:42 -0400

    adding online db

--- server/config/database.js
+++ server/config/database.js
@@ -1,1 +1,16 @@
 import pg from 'pg'
+
+//Connection Pool Object
+const config = {
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}
+//Notice that we're telling the config objects that itll find its data
+//in the environment variables, so we have to make them
+export const pool = new pg.Pool(config)
